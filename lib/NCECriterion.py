772d6c92260df5d8ca66c85ac16e89a89c4d2045
Lv HaoYu <frelam112233@gmail.com>
2018-12-24 13:30:28 +0800

    Update NCECriterion.py

--- lib/NCECriterion.py
+++ lib/NCECriterion.py
@@ -3,36 +3,81 @@ from torch import nn
 
 eps = 1e-7
 
-class NCECriterion(nn.Module):
+
+class NCEGroupCriterion(nn.Module):
 
     def __init__(self, nLem):
-        super(NCECriterion, self).__init__()
+        super(NCEGroupCriterion, self).__init__()
         self.nLem = nLem
 
     def forward(self, x, targets):
         batchSize = x.size(0)
-        K = x.size(1)-1
+        K = x.size(1) - 1
+        K1 = 100
+        # x.size() 包括了positive和negative samples的数目。
+        # K为 negative sample 的数目。
         Pnt = 1 / float(self.nLem)
+        # Pnt 为 Pn(i) = 1/n
         Pns = 1 / float(self.nLem)
-        
-        # eq 5.1 : P(origin=model) = Pmt / (Pmt + k*Pnt) 
-        Pmt = x.select(1,0)
-        Pmt_div = Pmt.add(K * Pnt + eps)
-        lnPmt = torch.div(Pmt, Pmt_div)
-        
-        # eq 5.2 : P(origin=noise) = k*Pns / (Pms + k*Pns)
-        Pon_div = x.narrow(1,1,K).add(K * Pns + eps)
-        Pon = Pon_div.clone().fill_(K * Pns)
-        lnPon = torch.div(Pon, Pon_div)
-     
+        lnPmtsum = 0
+        lnPonsum = 0
+        min_prob_of_group = 0.003
+
+        for i in range(batchSize):
+
+            for j in range(1,5):
+                if x[i][j] < min_prob_of_group:
+                    break
+
+            #j =1
+            for index in range(j):
+                #if index > int(10):
+                    #break
+                # eq 5.1 : P(origin=model) = Pmt / (Pmt + k*Pnt)
+                if index == 0:
+                    Pmt = x[i].select(0, index)
+                    Pmt_div = Pmt.add(K1 * Pnt + eps)
+                    lnPmt = torch.div(Pmt, Pmt_div).log_()
+                else:
+                    Pmt = x[i].select(0, index)
+                    Pmt_div = Pmt.add(K1 * Pnt + eps)
+                    lnPmt_temp = torch.div(Pmt, Pmt_div).log_()
+                    lnPmt = torch.add(lnPmt, lnPmt_temp)
+                # Pmt 即公式里的 P(i|v)
+
+                if i == 0:
+                    lnPmtsum = lnPmt
+                else:
+                    lnPmtsum = torch.add(lnPmtsum,lnPmt)
+                # lnPmt = h(i|v) = P(i|v) / P(i|v) + m*Pn(i)
+            '''
+                if index == 0:
+                    lnPmt_total = lnPmt.log_()
+                #lnPon_total = lnPon.log_()
+                else:
+                    lnPmt_total.add(lnPmt.log_())
+                #lnPon_total.add(lnPon.log_())
+            '''
+            # eq 5.2 : P(origin=noise) = k*Pns / (Pms + k*Pns)
+            Pon_div = x[i].narrow(0, j, int(K-j)).add(K1 * Pns + eps)
+            Pon = Pon_div.clone().fill_(K1 * Pns)
+            lnPon = torch.div(Pon, Pon_div)
+            lnPon.log_()
+
+            lnPon = lnPon.sum(0)
+            if i == 0:
+                lnPonsum = lnPon
+            else:
+                lnPonsum = torch.add(lnPonsum,lnPon)
+        # h(i|v')
+
         # equation 6 in ref. A
-        lnPmt.log_()
-        lnPon.log_()
-        
-        lnPmtsum = lnPmt.sum(0)
-        lnPonsum = lnPon.view(-1, 1).sum(0)
-        
-        loss = - (lnPmtsum + lnPonsum) / batchSize
-        
+
+
+        #lnPmtsum = lnPmt.sum(0)
+        #lnPonsum = lnPon.view(-1, 1).sum(0)
+
+        loss = - (lnPmtsum + lnPonsum )/ batchSize
+        # loss = -E{log(h(i|v))} - E{log(1 - h(i|v'))}
         return loss
 
